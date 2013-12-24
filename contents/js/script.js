c1d45a3c8c06fda601c57597036f6f8d8fef03c7
Paul C Pederson <ppederson@esri.com>
2013-12-24 15:19:58 -0800

    add ready class to body on page load for later use in loading animations.

--- contents/js/script.js
+++ contents/js/script.js
@@ -1,1 +1,4 @@
-console.log("super!");
+//Add ready class to body for onload animations
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByTagName("body")[0].className += ' ready';
+});
