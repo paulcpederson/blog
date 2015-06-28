874df190e8aa55cc008659fa1ed1ed5a3b27ac23
Paul C Pederson <paul.c.pederson@gmail.com>
2015-06-28 14:52:19 -0700

    archive page

--- source/_helpers/articles.js
+++ source/_helpers/articles.js
@@ -3,9 +3,25 @@ function isArticle (page) {
 }
 
 module.exports = function (site, cb) {
-  var articles = site.filter(isArticle).sort(function (a, b) {
-    return new Date(b.date) - new Date(a.date)
-  })
+  var articles = site
+    .filter(isArticle)
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date)
+    })
+    .map(function (article) {
+      var year = article.date.getFullYear()
+      return {
+        title: article.title,
+        year: year,
+        date: article.date,
+        canvas: article.canvas,
+        description: article.description,
+        spot: article.spot,
+        thumbnail: article.thumbnail,
+        background: article.background,
+        url: article.url
+      }
+    })
   site = site.map(function (page) {
     page.articles = articles
     if (isArticle(page)) {
