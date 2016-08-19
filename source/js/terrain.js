70b1706bd8554b32f7ea5a6ffb993ceb16ab0ac5
Paul C Pederson <paul.c.pederson@gmail.com>
2016-08-19 12:29:33 -0700

    initial version of terrain

--- source/js/terrain.js
+++ source/js/terrain.js
@@ -3,13 +3,29 @@ import colors from './lib/palette'
 const canvas = document.querySelector('.js-blob-canvas')
 const context = canvas.getContext('2d')
 
-var width = canvas.width = window.innerWidth
+const grays = {
+  lightest: '#FFFFFF',
+  lighter: '#FFFFF8',
+  light: '#FFFCF2',
+  0: '#F5F2E8',
+  dark: '#E5E3DA',
+  darker: '#DBD9D0',
+  darkest: '#D1CFC6'
+}
+
+let width = canvas.width = window.innerWidth
 let height = canvas.height = window.innerHeight
+let mouse = {x: width / 2, y: height / 2}
 
 window.onresize = () => {
   width = canvas.width = window.innerWidth
   height = canvas.height = window.innerHeight
 }
+
+window.addEventListener('mousemove', e => {
+  mouse.x = e.clientX
+  mouse.y = e.clientY
+})
 
 function distance (a, b) {
   var dx = a[0] - b[0]
@@ -25,25 +41,46 @@ class Vertex {
     this.y = y
     this.originX = x
     this.originY = y
-    this.dx = deltas[Math.floor(Math.random() * deltas.length)]
-    this.dy = deltas[Math.floor(Math.random() * deltas.length)]
   }
   update () {
-    if (distance([this.x, this.y], [this.originX, this.originY]) > 20) {
-      this.dx = -this.dx
-      this.dy = -this.dy
+    let delta = distance([mouse.x, mouse.y], [this.originX, this.originY])
+    if (delta < 150) {
+      this.y = this.originY - (100 - delta)
     }
-    this.x = this.x - this.dx
-    this.y = this.y - this.dy
+    if (this.x > this.originX) {
+      this.x = this.x - 1
+    }
+    if (this.x < this.originX) {
+      this.x = this.x + 1
+    }
+    if (this.y > this.originY) {
+      this.y = this.y - 1
+    }
+    if (this.y < this.originY) {
+      this.y = this.y + 1
+    }
+    // if (this.x == this.originX && this.y == this.originY) {
+    //   this.x = this.originX + (100 * Math.random())
+    //   this.y = this.originY + (100 * Math.random())
+    // }
   }
 }
 
 class Triangle {
-  constructor (a, b, c) {
-    this.a = a
-    this.b = b
-    this.c = c
+  constructor (options) {
+    this.a = options.a
+    this.b = options.b
+    this.c = options.c
+    this.fill = colors.base02
+    this.orientation = options.orientation
+
   }
+
+  // shade the triangle based on slope
+  shade () {
+
+  }
+
 }
 
 let vertices = []
@@ -54,9 +91,9 @@ const columns = Math.floor((width + columnWidth) / columnWidth)
 const rows = Math.floor((height + rowHeight) / rowHeight)
 
 // generate vertices
-for (let i = -1; i < rows + 1; i++) {
+for (let i = -1; i < rows + 2; i++) {
   let row = []
-  for (let j = -1; j < columns + 1; j++) {
+  for (let j = -1; j < columns + 2; j++) {
     let offset = i % 2 ? columnWidth / 2 : 0
     row.push(new Vertex(j * columnWidth - offset, i * rowHeight))
   }
@@ -71,11 +108,11 @@ for (let i = 0; i < vertices.length; i++) {
     for (let j = 0; j < row.length; j++){
       if (row[j + 1] && nextRow[j + 1]) {
         if (i % 2 === 0) {
-          triangles.push(new Triangle(row[j], row[j + 1], nextRow[j]))
-          triangles.push(new Triangle(row[j + 1], nextRow[j], nextRow[j + 1]))
+          triangles.push(new Triangle({ a: row[j], b: row[j + 1], c: nextRow[j], orientation: 'south'}))
+          triangles.push(new Triangle({ a: row[j + 1], b: nextRow[j + 1], c: nextRow[j], orientation: 'north'}))
         } else {
-          triangles.push(new Triangle(row[j], nextRow[j], nextRow[j+1]))
-          triangles.push(new Triangle(row[j], row[j+1], nextRow[j + 1]))
+          triangles.push(new Triangle({ a: row[j], b: nextRow[j + 1], c: nextRow[j], orientation: 'north'}))
+          triangles.push(new Triangle({ a: row[j], b: row[j + 1], c: nextRow[j + 1], orientation: 'south'}))
         }
       }
     }
@@ -87,7 +124,7 @@ function loop () {
   context.clearRect(0, 0, width, height)
 
   triangles.forEach(t => {
-    context.fillStyle = colors.base02
+    context.fillStyle = t.fill
     context.strokeStyle = colors.blue
     context.beginPath()
     context.moveTo(t.a.x, t.a.y)
