5140718c378b6c201a96b564bc0f67dfa7b4b658
Paul C Pederson <paul.c.pederson@gmail.com>
2016-09-13 21:14:50 -0700

    get transitions working

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,28 @@
-// Responsive Tables
+import * as barba from 'barba.js'
+
+var FadeTransition = barba.BaseTransition.extend({
+  start: function() {
+    console.log(this.oldContainer)
+    this.oldContainer.classList.add('fade-out')
+    window.scrollTo(0, 0)
+    this.newContainerLoading.then(this.done.bind(this))
+  }
+});
+
+/**
+ * Next step, you have to tell Barba to use the new Transition
+ */
+
+barba.Pjax.getTransition = function() {
+  return FadeTransition;
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   var tables = document.getElementsByTagName('table')
   for (var i = 0; i < tables.length; i++) {
     tables[i].outerHTML = '<div class="overflow-auto">' + tables[i].outerHTML + '</div>'
+  }
+  if (window.history.pushState) {
+    barba.Pjax.start()
   }
 })
