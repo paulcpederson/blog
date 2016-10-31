7f1a06043a46bd48846ef7d349d09159a8eb2af1
Paul C Pederson <paul.c.pederson@gmail.com>
2016-10-31 09:46:14 -0700

    initial low poly code working with random values

--- source/js/lowPoly.js
+++ source/js/lowPoly.js
@@ -6,27 +6,63 @@ camera.position.z = 1000
 var scene = new THREE.Scene()
 scene.background = new THREE.Color(0xf1f9f2)
 
-var geometry = new THREE.IcosahedronGeometry(300, 0)
 var material = new THREE.MeshBasicMaterial({
   color: 0xef7654,
   wireframe: true
 })
 
-var mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
 var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.domElement.classList.add('fullscreen-canvas')
-document.body.appendChild(renderer.domElement)
+document.querySelector('.js-lab').appendChild(renderer.domElement)
 
-function animate() {
-    requestAnimationFrame(animate)
+window.addEventListener('resize', e => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
-    mesh.rotation.x += 0.01
-    mesh.rotation.y += 0.02
+var geo = new THREE.IcosahedronGeometry(300, 1)
+geo.computeBoundingBox()
 
-    renderer.render(scene, camera)
+var cX = 0.5 * (geo.boundingBox.max.x - geo.boundingBox.min.x)
+var cY = 0.5 * (geo.boundingBox.max.y - geo.boundingBox.min.y)
+var cZ = 0.5 * (geo.boundingBox.max.z - geo.boundingBox.min.z)
+var center = new THREE.Vector3(cX, cY, cZ)
+
+var icosahedron = new THREE.Mesh(geo, material)
+var rotation = {
+  x: 0,
+  y: 0
+}
+
+function animate () {
+  scene.remove(icosahedron)
+
+  // create a new geometry and compute the bounding box
+  geo = new THREE.IcosahedronGeometry(300, 1)
+
+  rotation.x += 0.01
+  rotation.y += 0.02
+
+  // TODO: set programatically based on spectrum analysis
+  var distance = 1 + (Math.random() * .25)
+
+  geo.vertices = geo.vertices.map((v, i) => {
+    var newPos = new THREE.Vector3()
+    newPos.addVectors(center, v.multiplyScalar(distance))
+    return newPos
+  })
+
+  geo.center()
+
+  icosahedron = new THREE.Mesh(geo, material)
+  icosahedron.rotation.x = rotation.x
+  icosahedron.rotation.y = rotation.y
+
+  scene.add(icosahedron)
+
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
 }
 
 animate()
