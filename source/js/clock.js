e2df0f41f6a5075298f658426ed68c0dfebd2f38
Paul C Pederson <paul.c.pederson@gmail.com>
2016-11-23 11:10:56 -0800

    finish clock lab

--- source/js/clock.js
+++ source/js/clock.js
@@ -1,1 +1,7 @@
-// TODO: set initial position based on the current time
+let date = new Date()
+let seconds = date.getSeconds() * 6
+let minutes = date.getMinutes() * 6
+let hours = (date.getHours() * 30) + (minutes / 12)
+document.querySelector('.js-second').style.transform = `rotate(${seconds}deg)`
+document.querySelector('.js-minute').style.transform = `rotate(${minutes}deg)`
+document.querySelector('.js-hour').style.transform = `rotate(${hours}deg)`
