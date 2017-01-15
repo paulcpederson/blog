25af10ba87c5881006b09b6453f0235450f8b38b
Paul C Pederson <paul.c.pederson@gmail.com>
2017-01-15 11:00:01 -0800

    working tooltips in the network graph

--- source/js/genres.js
+++ source/js/genres.js
@@ -1,28 +1,23 @@
 import music from './data/music'
-// import d3 from 'd3'
-// import forceBundle from './lib/forceBundle'
-var $svg = document.querySelector('.js-container-svg')
-console.log($svg)
-var svg = d3.select($svg)
-var width = window.innerWidth
-var height = window.innerHeight
+import * as d3 from 'd3'
 
-// var color = d3.scaleOrdinal(d3.schemeCategory20);
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.name; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
-
-var edges = []
-var length = music.artists.length
+let svg = d3.select('.js-container-svg')
+let tooltip = d3.select('.js-tooltip')
+let width = window.innerWidth
+let height = window.innerHeight
+let length = music.artists.length
+let edges = []
+let active = ''
+let transform = 'translate(0, 0)'
+let scale = 1
 
 for (let i = 0; i < length; i++) {
-  var artist1 = music.artists[i]
+  let artist1 = music.artists[i]
   for (let j = 0; j < length; j++) {
-    var column = music.artists[j];
+    let column = music.artists[j];
     if (i < j) {
-      var artist2 = music.artists[j]
-      var score = artist1.genres.reduce((prev, curr) => {
+      let artist2 = music.artists[j]
+      let score = artist1.genres.reduce((prev, curr) => {
         return artist2.genres.indexOf(curr) > -1 ? prev + 1 : prev
       }, 0);
       if (score > 2) {
@@ -36,74 +31,98 @@ for (let i = 0; i < length; i++) {
   }
 }
 
-var tooltip = d3.select('.js-tooltip')
+let simulation = d3.forceSimulation()
+    .force('link', d3.forceLink().id(d => d.name))
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2));
 
-var link = svg.append("g")
-    .attr("class", "links")
-  .selectAll("line")
+let link = svg.append('g')
+  .attr('class', 'links')
+  .selectAll('line')
   .data(edges)
-  .enter().append("line")
-    .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+  .enter()
+  .append('line')
 
-var node = svg.append("g")
-    .attr("class", "nodes")
-  .selectAll("circle")
+let node = svg.append('g')
+  .attr('class', 'nodes')
+  .selectAll('circle')
   .data(music.artists)
   .enter()
-  .append("circle")
-    .attr("r", 3)
-    .attr("fill", "white")
+  .append('circle')
+    .attr('r', 3)
+    .attr('fill', 'white')
     .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended))
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended))
 
 node
-  .on("mouseover", (e) => {
-    tooltip.html(e.name)
-    return tooltip.style("visibility", "visible")
-  })
-  .on("mousemove", (e) => tooltip.style("top", `${d3.event.pageY - 10}px`).style("left",(d3.event.pageX+10)+"px"))
-  .on("mouseout", () => tooltip.style("visibility", "hidden"))
+  .on('mouseover', onHover)
+  .on('mouseout', onHoverOut)
 
-svg.call(d3.zoom().on("zoom", function () {
-  node.attr("transform", "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ") scale(" + d3.event.transform.k + ")");
-  link.attr("transform", "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ") scale(" + d3.event.transform.k + ")");
-}))
-
+svg.call(d3.zoom().on('zoom', onZoom))
 
 simulation
     .nodes(music.artists)
-    .on("tick", ticked);
+    .on('tick', ticked)
 
-simulation.force("link")
-    .links(edges);
+simulation.force('link')
+    .links(edges)
 
-function ticked() {
+function ticked () {
   link
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-
+    .attr('x1', d => d.source.x)
+    .attr('y1', d => d.source.y)
+    .attr('x2', d => d.target.x)
+    .attr('y2', d => d.target.y)
+    .attr()
   node
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
 }
 
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+let links = d3.select('.links')
+let nodes = d3.select('.nodes')
+
+function onHover (d) {
+  active = d.name
+  return tooltip
+    .html(d.name)
+    .style('top', `${d.y * scale - 17}px`)
+    .style('left', `${d.x * scale + 14}px`)
+    .style('transform', transform)
+    .classed('is-active', true)
 }
 
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
+function onHoverOut (d) {
+  return tooltip.classed('is-active', false);
 }
 
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+function onZoom () {
+  let {x, y, k} = d3.event.transform
+  scale = k
+  transform = `translate(${x}px, ${y}px)`
+  nodes.attr('transform', `translate(${x}, ${y}) scale(${k})`)
+  links.attr('transform', `translate(${x}, ${y}) scale(${k})`)
+}
+
+function dragged (d) {
+  d.fx = d3.event.x
+  d.fy = d3.event.y
+}
+
+function dragstarted (d) {
+  if (!d3.event.active) {
+    simulation.alphaTarget(0.3).restart()
+  }
+  d.fx = d.x
+  d.fy = d.y
+}
+
+function dragended (d) {
+  if (!d3.event.active) {
+    simulation.alphaTarget(0)
+  }
+  d.fx = null
+  d.fy = null
 }
