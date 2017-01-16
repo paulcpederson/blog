830e5ddccd4db4abae0a6b749a7d1d7ad1c27fbb
Paul C Pederson <paul.c.pederson@gmail.com>
2017-01-15 17:09:34 -0800

    make a functioning info panel for all artists

--- source/js/genres.js
+++ source/js/genres.js
@@ -3,6 +3,11 @@ import * as d3 from 'd3'
 
 let svg = d3.select('.js-container-svg')
 let tooltip = d3.select('.js-tooltip')
+let searchInput = document.querySelector('.js-search-input')
+let suggestions = document.querySelector('.js-search-suggestions')
+let searchButton = document.querySelector('.js-search-button')
+let search = document.querySelector('.js-search')
+var info = document.querySelector('.js-search-info')
 let width = window.innerWidth
 let height = window.innerHeight
 let length = music.artists.length
@@ -10,6 +15,9 @@ let edges = []
 let active = ''
 let transform = 'translate(0, 0)'
 let scale = 1
+
+// TODO remove
+let img = 'https://lastfm-img2.akamaized.net/i/u/avatar170s/620abf38efaf498298196e3e06e6286a.jpg'
 
 for (let i = 0; i < length; i++) {
   let artist1 = music.artists[i]
@@ -55,6 +63,7 @@ let node = svg.append('g')
         .on('start', dragstarted)
         .on('drag', dragged)
         .on('end', dragended))
+    .on('click', click)
 
 node
   .on('mouseover', onHover)
@@ -83,6 +92,96 @@ function ticked () {
 
 let links = d3.select('.links')
 let nodes = d3.select('.nodes')
+
+function click (d) {
+  active = d.name;
+  showInfo(d);
+}
+
+info.addEventListener('click', function (e) {
+  if (e.target.classList.contains('similar__artist__link')) {
+    e.preventDefault()
+    let name = e.target.getAttribute('data-name')
+    let chosenArtist = music.artists.filter(artist => artist.name === name).shift()
+    showInfo(chosenArtist)
+  }
+})
+
+suggestions.addEventListener('click', function (e) {
+  let name = e.target.getAttribute('data-name')
+  let chosenArtist = music.artists.filter(artist => artist.name === name).shift()
+  showInfo(chosenArtist)
+})
+
+function showResults (term) {
+  search.classList.remove('is-active')
+  search.classList.remove('is-searching')
+  if (term.length > 0) {
+    suggestions.innerHTML = music.artists
+      .filter(artist => artist.name.toLowerCase().indexOf(term) > -1)
+      .slice(0, 5)
+      .map(artist => `<a href="#" class="search-result" data-name="${artist.name}"><img src="${img}" class="artist-thumb artist-thumb--small" alt="${artist.name}">${artist.name}</a>`)
+      .join('')
+    search.classList.add('is-searching')
+  }
+}
+
+searchInput.addEventListener('input', e => {
+  let searchTerm = e.target.value.toLowerCase()
+  showResults(searchTerm)
+})
+
+searchButton.addEventListener('click', e => {
+  let isClose = search.classList.contains('is-active') || search.classList.contains('is-searching')
+  if (isClose) {
+    search.classList.remove('is-active')
+    search.classList.remove('is-searching')
+    searchInput.value = ''
+    searchInput.focus()
+  } else {
+    showResults(searchInput.value)
+  }
+})
+
+function showInfo (d) {
+  search.classList.remove('is-searching')
+  searchInput.value = d.name;
+
+  let genres = d.genres.reduce((prev, curr) => {
+    return prev + `<li class="genre__tag">${curr}</li>`
+  }, '')
+
+  let similarArtists = edges
+    .filter(edge => edge.source.name === d.name || edge.target.name === d.name)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 4)
+    .map(edge => edge.source.name !== d.name ? edge.source : edge.target)
+    .map(artist => {
+      return `
+        <li class="similar__artist">
+          <a href="#" class="similar__artist__link" data-name="${artist.name}">
+            <img src="${img}" class="artist-thumb artist-thumb--small" alt="${artist.name}">${artist.name}
+          </a>
+        </li>`
+    })
+    .join('')
+
+  info.innerHTML = `
+    <div class="artist">
+      <img src="https://lastfm-img2.akamaized.net/i/u/avatar170s/620abf38efaf498298196e3e06e6286a.jpg" class="artist-thumb" alt="${d.name}">
+      <h2 class="text-center font-size-1 leader-half trailer-0">${d.name}</h2>
+    </div>
+    <div class="padding-left-1 padding-right-1 trailer-half">
+      <h4 class="leader-1 trailer-half font-size-0 text-light">Tagged</h4>
+      <ul class="genre">${genres}</ul>
+    </div>
+    <div class="padding-left-1 padding-right-1 trailer-half ${similarArtists.length > 0 ? '' : 'hide'}">
+      <h4 class="leader-1 trailer-half font-size-0 text-light">Similar To</h4>
+      <ul class="similar">${similarArtists}</ul>
+    </div>
+  `
+  search.classList.add('is-active');
+}
 
 function onHover (d) {
   active = d.name
